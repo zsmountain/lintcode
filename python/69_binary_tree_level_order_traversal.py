9b56cb162b3146a4a3732cf085ca1201bc9c93a5
szhao <szhao@factset.com>
2018-11-27 19:43:50 -0500

    69. Binary Tree Level Order Traversal

---
+++ python/69_binary_tree_level_order_traversal.py
@@ -0,0 +1,73 @@
+'''
+Description
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+Example
+Given binary tree {3,9,20,#,#,15,7},
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+ 
+
+return its level order traversal as:
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+Challenge
+Challenge 1: Using only 1 queue to implement it.
+
+Challenge 2: Use BFS algorithm to do it.
+'''
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+import helper
+TreeNode = helper.TreeNode
+createTree = helper.createTree
+
+from collections import deque
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Level order a list of lists of integer
+    """
+
+    def levelOrder(self, root):
+        # write your code here
+        res = []
+        if not root:
+            return res
+        queue = deque([])
+        queue.appendleft(root)
+        while queue:
+            row = []
+            for i in range(len(queue)):
+                node = queue.popleft()
+                row.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(row)
+        return res
+
+s = Solution()
+root = createTree([3, 9, 20, '#', '#', 15, 7])
+root.prettyPrint()
+print(s.levelOrder(root))
+
+root =createTree([1, 2, 3])
+root.prettyPrint()
+print(s.levelOrder(root))
--- python/helper.py
+++ python/helper.py
@@ -1,3 +1,4 @@
+################################### List ###################################
 class ListNode(object):
     def __init__(self, val, next=None):
         self.val = val
@@ -22,3 +23,265 @@ class LinkedList():
             print(head.val, '->', end = ' ')
             head = head.next
         print('None')
+
+
+################################### Tree ###################################
+from copy import deepcopy as deepcopy
+import sys
+
+class Queue(object):
+  def __init__(self, items=None):
+    if items is None:
+      self.a = []
+    else:
+      self.a = items
+
+  def enqueue(self, b):
+    self.a.insert(0, b)
+
+  def dequeue(self):
+    return self.a.pop()
+
+  def isEmpty(self):
+    return self.a == []
+
+  def size(self):
+    return len(self.a)
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+  def visit(self):
+    sys.stdout.write(self.val)
+
+  def getNumNodes(self):
+    total = 0
+    if self.left:
+      total += self.left.getNumNodes()
+    if self.right:
+      total += self.right.getNumNodes()
+    return total + 1
+
+  @classmethod
+  def createTree(cls, depth):
+    tree = TreeNode('X')
+    cls.createTreeHelper(tree, depth, 1)
+    return tree
+
+  @classmethod
+  def createTreeHelper(cls, node, depth, cur):
+    if cur == depth:
+      return
+    node.left = TreeNode('X')
+    node.right = TreeNode('XX')
+    cls.createTreeHelper(node.left, depth, cur + 1)
+    cls.createTreeHelper(node.right, depth, cur + 1)
+
+  def getHeight(self):
+    return TreeNode.getHeightHelper(self)
+
+  @staticmethod
+  def getHeightHelper(node):
+    if not node:
+      return 0
+    else:
+      return max(TreeNode.getHeightHelper(node.left), TreeNode.getHeightHelper(node.right)) + 1
+
+  def fillTree(self, height):
+    TreeNode.fillTreeHelper(self, height)
+
+  def fillTreeHelper(node, height):
+    if height <= 1:
+      return
+    if node:
+      if not node.left:
+          node.left = TreeNode(' ')
+      if not node.right:
+          node.right = TreeNode(' ')
+      TreeNode.fillTreeHelper(node.left, height - 1)
+      TreeNode.fillTreeHelper(node.right, height - 1)
+
+  def prettyPrint(self):
+    """
+    """
+    # get height of tree
+    total_layers = self.getHeight()
+
+    tree = deepcopy(self)
+
+    tree.fillTree(total_layers)
+    # start a queue for BFS
+    queue = Queue()
+    # add root to queue
+    queue.enqueue(tree)  # self = root
+    # index for 'generation' or 'layer' of tree
+    gen = 1
+    # BFS main
+    while not queue.isEmpty():
+      # copy queue
+      #
+      copy = Queue()
+      while not queue.isEmpty():
+        copy.enqueue(queue.dequeue())
+      #
+      # end copy queue
+
+      first_item_in_layer = True
+      edges_string = ""
+      extra_spaces_next_node = False
+
+      # modified BFS, layer by layer (gen by gen)
+      while not copy.isEmpty():
+
+        node = copy.dequeue()
+
+        # -----------------------------
+        # init spacing
+        spaces_front = pow(2, total_layers - gen + 1) - 2
+        spaces_mid = pow(2, total_layers - gen + 2) - 2
+        dash_count = pow(2, total_layers - gen) - 2
+        if dash_count < 0:
+          dash_count = 0
+        spaces_mid = spaces_mid - (dash_count*2)
+        spaces_front = spaces_front - dash_count
+        init_padding = 2
+        spaces_front += init_padding
+        if first_item_in_layer:
+          edges_string += " " * init_padding
+        # ----------------------------->
+
+        # -----------------------------
+        # construct edges layer
+        edge_sym = "/" if node.left and node.left.val is not " " else " "
+        if first_item_in_layer:
+          edges_string += " " * (pow(2, total_layers - gen) - 1) + edge_sym
+        else:
+          edges_string += " " * (pow(2, total_layers - gen + 1) + 1) + edge_sym
+        edge_sym = "\\" if node.right and node.right.val is not " " else " "
+        edges_string += " " * (pow(2, total_layers - gen + 1) - 3) + edge_sym
+        # ----------------------------->
+
+        # -----------------------------
+        # conditions for dashes
+        if node.left and node.left.val == " ":
+          dash_left = " "
+        else:
+          dash_left = "_"
+
+        if node.right and node.right.val == " ":
+          dash_right = " "
+        else:
+          dash_right = "_"
+        # ----------------------------->
+
+        # -----------------------------
+        # handle condition for extra spaces when node lengths don't match or are even:
+        if extra_spaces_next_node:
+          extra_spaces = 1
+          extra_spaces_next_node = False
+        else:
+          extra_spaces = 0
+        # ----------------------------->
+
+        # -----------------------------
+        # account for longer val
+        val_length = len(str(node.val))
+        if val_length > 1:
+          if val_length % 2 == 1:  # odd
+            if dash_count > 0:
+              dash_count -= ((val_length - 1)/2)
+            else:
+              spaces_mid -= (val_length - 1)/2
+              spaces_front -= (val_length - 1)/2
+              if val_length is not 1:
+                extra_spaces_next_node = True
+          else:  # even
+            if dash_count > 0:
+              dash_count -= ((val_length)/2) - 1
+              extra_spaces_next_node = True
+              # dash_count += 1
+            else:
+              spaces_mid -= (val_length - 1)
+              spaces_front -= (val_length - 1)
+        # ----------------------------->
+
+        # -----------------------------
+        # print node with/without dashes
+        if first_item_in_layer:
+          print((" " * spaces_front) + (dash_left * dash_count) + \
+              str(node.val) + (dash_right * dash_count), end = '')
+          first_item_in_layer = False
+        else:
+          print((" " * (spaces_mid-extra_spaces)) + (dash_left *
+                                                    dash_count) + str(node.val) + (dash_right * dash_count), end = '')
+        # ----------------------------->
+
+        if node.left:
+            queue.enqueue(node.left)
+        if node.right:
+            queue.enqueue(node.right)
+
+      # print the fun squiggly lines
+      if not queue.isEmpty():
+        print("\n" + edges_string)
+
+      # increase layer index
+      gen += 1
+    print()
+    
+def createTree(vals):
+    if not vals:
+        return None
+    node_list = []
+    root = TreeNode(vals[0])
+    node_list.append(root)
+    index = 0
+    is_left = True
+    for i in range(1, len(vals)):
+        val = vals[i]
+        if val != '#':
+            node = TreeNode(val)
+            if is_left:
+                node_list[index].left = node
+            else:
+                node_list[index].right = node
+            node_list.append(node)
+        if not is_left:
+            index += 1
+        is_left = not is_left
+
+    return root
+
+if __name__ == '__main__':
+  # prep the tree...
+  #
+  # layer 1
+  root = TreeNode('A')
+
+  # layer 2
+  root.left = TreeNode('B')
+  root.right = TreeNode('C')
+
+  # layer 3
+  root.left.left = TreeNode('D')
+  root.left.right = TreeNode('E')
+
+  root.left.right.right = TreeNode.createTree(2)
+
+  root.right.left = TreeNode('F')
+  root.right.right = TreeNode('G')
+
+  # layer 3
+  root.left.left.left = TreeNode('H')
+  root.left.left.right = TreeNode('I')
+  root.left.right.left = TreeNode('J')
+  # root.left.right.right = TreeNode('K')
+  # root.right.left.left = TreeNode('L')
+  # root.right.left.right = TreeNode('M')
+  root.right.right.left = TreeNode('N')
+  root.right.right.right = TreeNode('O')
+
+  root.prettyPrint()
