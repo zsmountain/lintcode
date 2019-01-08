36b87c11ea4011daf623f52949287c9b5dd4d881
zsmountain27@gmail.com <zsmountain27@gmail.com>
2019-01-07 23:28:26 -0500

    88. Lowest Common Ancestor of a Binary Tree

---
+++ python/dfs/88_lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,71 @@
+'''
+Given the root and two nodes in a Binary Tree. Find the lowest common ancestor(LCA) of the two nodes.
+
+The lowest common ancestor is the node with largest depth which is the ancestor of both nodes.
+
+Assume two nodes are exist in tree.
+
+Have you met this question in a real interview?  
+Example
+For the following binary tree:
+
+  4
+ / \
+3   7
+   / \
+  5   6
+LCA(3, 5) = 4
+
+LCA(5, 6) = 7
+
+LCA(6, 7) = 7
+
+Related Problems
+'''
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+def load_src(name, fpath):
+    import os
+    import imp
+    return imp.load_source(name, os.path.join(os.path.dirname(__file__), fpath))
+
+load_src("helper", "../helper.py")
+from helper import TreeNode, createTree
+
+class Solution:
+    """
+    @param: root: The root of the binary search tree.
+    @param: A: A TreeNode in a Binary.
+    @param: B: A TreeNode in a Binary.
+    @return: Return the least common ancestor(LCA) of the two nodes.
+    """
+
+    def lowestCommonAncestor(self, root, A, B):
+        # write your code here
+        if not root:
+            return None
+        if root == A or root == B:
+            return root
+        left = self.lowestCommonAncestor(root.left, A, B)
+        right = self.lowestCommonAncestor(root.right, A, B)
+        if left and right:
+            return root
+        if left:
+            return left
+        if right:
+            return right
+        return None
+
+
+s = Solution()
+root = createTree([4, 3, 7, '#', '#', 5, 6])
+print(s.lowestCommonAncestor(root, root.left, root.right.left).val)
+print(s.lowestCommonAncestor(root, root.right.left, root.right.right).val)
+print(s.lowestCommonAncestor(root, root.right, root.right.right).val)
+        
