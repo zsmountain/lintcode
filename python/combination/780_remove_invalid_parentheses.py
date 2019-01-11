637f93b54771f4af9a454192e24fff4b3ab23d71
zsmountain27@gmail.com <zsmountain27@gmail.com>
2019-01-10 19:31:06 -0500

    780. Remove Invalid Parentheses

--- python/combination/780_remove_invalid_parentheses.py
+++ python/combination/780_remove_invalid_parentheses.py
@@ -18,30 +18,45 @@ class Solution:
 
     def removeInvalidParentheses(self, s):
         # Write your code here
-        open_left, left, right = 0, 0, 0
+        left, right = 0, 0
         for ch in s:
             if ch == '(':
                 left += 1
-                open_left += 1
             if ch == ')':
-                right += 1
-                open_left -= 1
+                if left > 0:
+                    left -= 1
+                else:
+                    right += 1
         res = []
-        self.helper(s, 0, res, left, right, open_left)
+        self.helper(s, 0, res, left, right)
         return res
 
-    def helper(self, s, index, res, left, right, open_left):
-        if index == len(s):
+    def helper(self, s, index, res, left, right):
+        if left == 0 and right == 0 and self.is_valid(s):
             res.append(s)
             return
         for i in range(index, len(s)):
+            if i > index and s[i] == s[i-1]:
+                continue
             ch = s[i]
-            if open_left <= 0 and ch == ')':
-                self.helper(s[:i] + s[i+1:], i, res, left, right - 1, open_left + 1)
-            if left > right and ch == '(':
-                self.helper(s[:i] + s[i+1:], i, res, left - 1, right, open_left - 1)
-            if left < right and ch == ')':
-                self.helper(s[:i] + s[i+1:], i, res, left, right - 1, open_left + 1)
+            if left > 0 and ch == '(':
+                self.helper(s[:i] + s[i+1:], i, res, left - 1, right)
+            if right > 0 and ch == ')':
+                self.helper(s[:i] + s[i+1:], i, res, left, right - 1)
+
+    def is_valid(self, s):
+        count = 0
+        for ch in s:
+            if ch == '(':
+                count += 1
+            if ch == ')':
+                if count <= 0:
+                    return False
+                count -= 1
+        return count == 0
 
 s = Solution()
+print(s.removeInvalidParentheses(')('))
+print(s.removeInvalidParentheses('((())))()(('))
+print(s.removeInvalidParentheses('()'))
 print(s.removeInvalidParentheses('()())()'))
