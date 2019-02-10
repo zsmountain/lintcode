344c501998d273ed3b4258608ab1114c356bed17
zsmountain27@gmail.com <zsmountain27@gmail.com>
2019-02-10 10:10:49 -0500

    172. Remove Element

---
+++ python/172_remove_element.py
@@ -0,0 +1,38 @@
+'''
+Given an array and a value, remove all occurrences of that value in place and return the new length.
+
+The order of elements can be changed, and the elements after the new length don't matter.
+
+Have you met this question in a real interview?  
+Example
+Example 1:
+	Input: [], value = 0
+	Output: 0
+
+
+Example 2:
+	Input:  [0,4,4,0,0,2,4,4], value = 4
+	Output: 4
+	
+	Explanation: 
+	the array after remove is [0,0,0,2]
+'''
+
+class Solution:
+    """
+    @param: A: A list of integers
+    @param: elem: An integer
+    @return: The new length after remove
+    """
+
+    def removeElement(self, A, elem):
+        # write your code here
+        j = len(A) - 1
+        for i in range(len(A) - 1, -1, -1):
+            if A[i] == elem:
+                A[i], A[j] = A[j], A[i]
+                j -= 1
+        return j + 1
+
+s = Solution()
+print(s.removeElement([0, 4, 4, 0, 0, 2, 4, 4], 4))
